97a109f40002a67f1848c427649baaf46ac6fbca
Ritesh Kumar <rkritesh078@gmail.com>
2015-08-09 15:14:40 +0530

    server created

---
+++ index.js
@@ -0,0 +1,60 @@
+var app = require('express')();
+var io = require('socket.io').(app.listen(3000));
+var five = require('johnny-five');
+
+//Setting the path to static assets
+app.use(express.static(__dirname + '/app'));
+
+//Serving the static HTML file
+app.get('/', function (res) {
+    res.sendFile('/index.html')
+});
+
+var board = new five.Board({
+    repl: false
+});
+
+io.on('connection', function (socket) {
+    board.on('ready', function () {
+        var speed, commands, motors;
+        motors = {
+            a: new five.Motor([3, 12]),
+            b: new five.Motor([11, 13])
+        };
+
+        commands = null;
+        speed = 255;
+
+        socket.on('stop', function () {
+            speed = 255;
+            motors.a.fwd(speed);
+            motors.b.fwd(speed);
+        });
+
+        socket.on('start', function () {
+            speed = 150;
+            motors.a.fwd(speed);
+            motors.b.fwd(speed);
+        });
+
+        socket.on('reverse', function () {
+            speed = 120;
+            motors.a.rev(speed);
+            motors.b.rev(speed);
+        });
+
+        socket.on('left', function () {
+            var aSpeed = 220;
+            var bSpeed = 50;
+            motors.a.fwd(aSpeed);
+            motors.b.rev(bSpeed);
+        });
+
+        socket.on('right', function () {
+            var aSpeed = 50;
+            var bSpeed = 220;
+            motors.a.rev(aSpeed);
+            motors.b.fwd(bSpeed);
+        });
+    })
+});
