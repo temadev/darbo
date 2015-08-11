6570069c5fc830aa67a0f143a9b1c106c9856776
Ritesh Kumar <rkritesh078@gmail.com>
2015-08-12 02:02:24 +0530

    angular app started

---
+++ app/app.js
@@ -0,0 +1,22 @@
+angular.module('nodebot',[])
+    .controller('NbController',['$scope',function($scope){
+        var socket = io();
+
+        $scope.movements = {
+            moveForward : function(){
+                socket.emit('start');
+            },
+            moveBackward : function(){
+                socket.emit('reverse');
+            },
+            turnRight : function(){
+                socket.emit('right');
+            },
+            turnLeft : function(){
+                socket.emit('left');
+            },
+            stop : function(){
+                socket.emit('stop');
+            }
+        }
+    }]);
