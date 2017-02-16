e8868daca43431c54a9729d11ada614e552329e6
DaboFondation <temami@protonmail.com>
2017-02-16 23:27:12 +0300

    new methods

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var socket = io();
 
 function moveForward(){
-    socket.emit('start');
+    socket.emit('forward');
 }
 
 function turnRight(){
@@ -20,8 +20,28 @@ function stop(){
     socket.emit('stop');
 }
 
+function leftWheel(){
+    socket.emit('left-wheel');
+}
+
+function rightWheel(){
+    socket.emit('right-wheel');
+}
+
+function off(){
+    socket.emit('off');
+}
+
+function onForward(){
+    socket.emit('on-forward');
+}
+
 document.getElementById('forward').onclick = moveForward;
 document.getElementById('right').onclick = turnRight;
 document.getElementById('left').onclick = turnLeft;
 document.getElementById('reverse').onclick = moveReverse;
 document.getElementById('stop').onclick = stop;
+document.getElementById('left-wheel').onclick = leftWheel;
+document.getElementById('right-wheel').onclick = rightWheel;
+document.getElementById('off').onclick = off;
+document.getElementById('on-forward').onclick = onForward;
--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var board = new five.Board({
 });
 
 board.on('ready', function () {
+    console.log('board ready');
+
     var speed, commands, motors;
     motors = {
         a: new five.Motor([3, 12]),
@@ -23,27 +25,38 @@ board.on('ready', function () {
     };
 
     commands = null;
-    speed = 255;
+    //speed = 255;
+
+    speed = 120;
+    motors.a.rev(speed);
+    motors.b.rev(speed);
+    motors.a.stop();
+    motors.b.stop();
 
     io.on('connection', function (socket) {
+
         socket.on('stop', function () {
+            console.log('stop');
             motors.a.stop();
             motors.b.stop();
         });
 
-        socket.on('start', function () {
+        socket.on('forward', function () {
+            console.log('forward');
             speed = 150;
             motors.a.fwd(speed);
             motors.b.fwd(speed);
         });
 
         socket.on('reverse', function () {
+            console.log('reverse');
             speed = 120;
             motors.a.rev(speed);
             motors.b.rev(speed);
         });
 
         socket.on('left', function () {
+            console.log('left');
             var aSpeed = 220;
             var bSpeed = 50;
             motors.a.fwd(aSpeed);
@@ -51,10 +64,68 @@ board.on('ready', function () {
         });
 
         socket.on('right', function () {
+            console.log('right')
             var aSpeed = 50;
             var bSpeed = 220;
             motors.a.rev(aSpeed);
             motors.b.fwd(bSpeed);
+        });
+
+	socket.on('off', function () {
+	    console.log('off');
+	    speed = 120;
+            motors.a.rev(speed);
+            motors.b.rev(speed);
+            motors.a.stop();
+            motors.b.stop();
+	});
+
+	socket.on('on-forward', function () {
+	    console.log('on-forward');
+            speed = 120;
+            motors.a.rev(speed);
+            motors.b.rev(speed);
+            speed = 150;
+            motors.a.fwd(speed);
+            motors.b.fwd(speed);
+            motors.a.stop();
+            motors.b.stop();
+	});
+
+	socket.on('left-wheel', function () {
+	    console.log('left-wheel');
+            speed = 120;
+            motors.a.rev(speed);
+            motors.b.rev(speed);
+            motors.a.stop();
+            motors.b.stop();
+	    speed = 150;
+            motors.a.fwd(speed);
+            motors.b.fwd(speed);
+            var aSpeed = 220;
+            var bSpeed = 50;
+            motors.a.fwd(aSpeed);
+            motors.b.rev(bSpeed);
+            motors.a.stop();
+            motors.b.stop();
+	});
+
+        socket.on('right-wheel', function () {
+            console.log('right-wheel');
+            speed = 120;
+            motors.a.rev(speed);
+            motors.b.rev(speed);
+            motors.a.stop();
+            motors.b.stop();
+            speed = 150;
+            motors.a.fwd(speed);
+            motors.b.fwd(speed);
+            var aSpeed = 50;
+            var bSpeed = 220;
+            motors.a.rev(aSpeed);
+            motors.b.fwd(bSpeed);
+            motors.a.stop();
+            motors.b.stop();
         });
     });
 });
